9279e3bc05a2ac91d0f857e773b89cd029548dc3
Alexis-Ribeiro <131233947+Alexis-Ribeiro@users.noreply.github.com>
2023-05-05 16:59:17 +0930

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -23,7 +23,7 @@ pipeline {
             }
             post {
                 always {
-                        to:"alexis.ribeirog@hotmail.com",
+                        mail to:"alexis.ribeirog@hotmail.com",
                         subject:"Test Status: ${currentBuild.result}",
                         body:"The test stage has completed. Status: ${currentBuild.result}",
                         attachLog:true
@@ -42,7 +42,7 @@ pipeline {
             }
             post {
                 always {
-                        to:"alexis.ribeirog@hotmail.com",
+                        mail to:"alexis.ribeirog@hotmail.com",
                         subject:"Security Scan Status: ${currentBuild.result}",
                         body:"The security scan stage has completed. Status: ${currentBuild.result}",
                         attachLog:true
