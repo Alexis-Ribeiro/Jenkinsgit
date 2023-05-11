6f58a7d48d31026ffb6fd2a17903e770569ea1a5
Alexis-Ribeiro <131233947+Alexis-Ribeiro@users.noreply.github.com>
2023-05-11 15:54:42 +0930

    Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,72 +7,63 @@ pipeline {
     }
     stages {
         stage("Build") {
-            steps {
+            steps{
                 echo "Fetch the source code from the directory path: $DIRECTORY_PATH"                
                 echo "Compile the code and generate any necessary artifacts."
                 echo "Code built using a build automation tool called Maven."
-            }                      
+            }
+                            
         }
         stage("Unit and Integration Tests") {
-            steps {
+            steps{
                 echo "Unit tests."
                 echo "Integration tests."
-                echo "pytest was the tool used for this"                
+                echo "pytest was the tool used for this"
+                
             }
             post {
                 always {
-                    script {
-                        emailext subject: "Test Status: ${currentBuild.result}",
-                                  body: "The test stage has completed. Status: ${currentBuild.result}",
-                                  attachLog: true
-                    }
+                        mail to:"alexis.ribeirog@hotmail.com",
+                        subject:"Test Status: ${currentBuild.result}",
+                        body:"The test stage has completed. Status: ${currentBuild.result}",
+                        attachLog: true
                 }
             }
         }
         stage("Code Analysis") {
-            steps {
+            steps{
                 echo "Check the quality of the code"
                 echo "SonarQube was the tool used for this"
             }
         }
         stage("Security Scan") {
-            steps {
+            steps{
                 echo "Perform a security scan on the code using OWASP ZAP."
             }
             post {
                 always {
-                    script {
-                        emailext subject: "Security Scan Status: ${currentBuild.result}",
-                                  body: "The security scan stage has completed. Status: ${currentBuild.result}",
-                                  attachLog: true
-                    }
+                        mail to:"alexis.ribeirog@hotmail.com",
+                        subject:"Security Scan Status: ${currentBuild.result}",
+                        body:"The security scan stage has completed. Status: ${currentBuild.result}",
+                        attachLog: true
                 }
             }
         }
         stage("Deploy") {
-            steps {
+            steps{
                 echo "Deploy the application to a staging server (e.g., AWS EC2 instance) using Jenkins."
             }
         }
         stage("Integration Tests on Staging") {
-            steps {
-                echo "Run integration tests on the staging environment to ensure the application functions as expected in a production-like environment"
+            steps{
+                echo "run integration tests on the staging environment to ensure the application functions as expected in a production-like environment"
                 sleep 10
             }
         }
         stage("Deploy to Production") {
-            steps {
+            steps{
                 echo "The product $PRODUCTION_ENVIRONMENT is ready for production"
-                echo "Deploy the application to a production server (e.g., AWS EC2 instance) using Jenkins."
-            }
-            post {
-                always {
-                    script {
-                        emailext subject: "Production Deployment Status: ${currentBuild.result}",
-                                  body: "The deployment to production stage has completed. Status: ${currentBuild.result}",
-                                  attachLog: true
-                    }
-                }
+                echo "deploy the application to a production server (e.g., AWS EC2 instance) using Jenkins."
             }
         }
     }
